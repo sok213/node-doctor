cbe7f798e8e305b87dafb81b9146d3d7d7d80abf
sophanarith sok <sophanarithsok@gmail.com>
2017-05-22 12:44:18 -0700

    Clean up GET / route.

--- routes/index.js
+++ routes/index.js
@@ -9,12 +9,6 @@ const router = express.Router();
 // Public route for home page.
 router.get('/', (req, res) => {
   console.log(res.locals);
-  // Find all books posted by every user and send it back to client.
-  User.find({}, ['-email', '-city', '-state'], (err, doc) => {
-    res.render('home', {
-      books: doc
-    });
-  });
 });
 
 // Public route for sign up.
